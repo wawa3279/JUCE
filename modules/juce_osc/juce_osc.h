d3d60ea89ee77f463da22be9b019484f85a3b935
Tom Poole <tom@juce.com>
2024-03-26 08:04:02 +0000

    Bump version number to 7.0.11

--- modules/juce_osc/juce_osc.h
+++ modules/juce_osc/juce_osc.h
@@ -28,7 +28,7 @@
 
   ID:                 juce_osc
   vendor:             juce
-  version:            7.0.10
+  version:            7.0.11
   name:               JUCE OSC classes
   description:        Open Sound Control implementation.
   website:            http://www.juce.com/juce
